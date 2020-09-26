71b4c5bc4c430b9a7a3aae7a6aef922bcbccb017
Andrey Demchuk <avdemchuk@gmail.com>
2020-09-26 15:21:37 +0300

    Empty app module

---
+++ app/src/main/java/com/chilik1020/itacademyhw/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chilik1020.itacademyhw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
