3c416068827e8d843dcad8788d744877cab3f88f
Andrey Demchuk <avdemchuk@gmail.com>
2020-11-25 15:07:42 +0300

    Fixed Settings icon position

--- hw8/src/main/java/com/chilik1020/hw8/ContactApp.kt
+++ hw8/src/main/java/com/chilik1020/hw8/ContactApp.kt
@@ -1,14 +1,10 @@
 package com.chilik1020.hw8
 
 import android.app.Application
-import android.content.Context
 import com.chilik1020.hw8.di.appModule
 import com.chilik1020.hw8.di.interactorModule
 import com.chilik1020.hw8.di.roomModule
 import com.chilik1020.hw8.di.viewModelModule
-import com.chilik1020.hw8.util.REPOSITORY_TYPE_KEY
-import com.chilik1020.hw8.util.SHARED_PREF_NAME
-import com.chilik1020.hw8.util.TYPE_HANDLER
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -18,11 +14,6 @@ class ContactApp : Application() {
     override fun onCreate() {
         super.onCreate()
         initKoin()
-
-        applicationContext.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
-            .edit()
-            .putString(REPOSITORY_TYPE_KEY, TYPE_HANDLER)
-            .apply()
     }
 
     private fun initKoin() {
